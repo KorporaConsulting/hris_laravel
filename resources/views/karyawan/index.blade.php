a58bd5e663e45a810681bd1d49baae035d9011ff
ramaramarama009 <ramaramarama009@gmail.com>
2022-03-30 16:12:21 +0700

    Perbaikan responsive

--- resources/views/karyawan/index.blade.php
+++ resources/views/karyawan/index.blade.php
@@ -6,12 +6,12 @@
             <div class="card">
                 <div class="card-body">
                     <div class="row">
-                        <div class="col-3">
+                        <div class="col-12 col-lg-3">
                             <figure class="avatar mr-2 avatar-xl">
                                 <img src="/img/avatar/avatar-1.png" alt="...">
                             </figure>
                         </div>
-                        <div class="col-9 ">
+                        <div class="col-12 col-lg-9">
                             <div class="row">
                                 <div class="col-6 mb-3">
                                         <div class="font-weight-bold">Name</div>
